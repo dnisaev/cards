1153335f62f0b389c7cc2405aa7692cdac87c706
dnisaev <dnisaev@yandex.ru>
2024-04-16 17:24:56 +0300

    refactor examples array

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,6 @@ export function App() {
     { label: 'Fourth value', value: 'fourth' },
     { label: 'Fifth value', value: 'fifth' },
   ]
-
   const selectExampleArray = [
     { text: 'First select value', value: 'first' },
     { text: 'Second select value', value: 'second' },
@@ -23,98 +22,102 @@ export function App() {
     { text: 'Fourth select value', value: 'fourth' },
     { text: 'Fifth select value', value: 'fifth' },
   ]
-
   const tabsExampleArray = [
-    { title: 'Sign in', value: 'sign-in' },
-    { title: 'Radio Group', value: 'radio-group' },
-    { title: 'Select', value: 'select' },
-    { title: 'Checkbox', value: 'checkbox' },
-    { title: 'Button', value: 'button' },
+    { disabled: false, title: 'Sign in', value: 'sign-in' },
+    { disabled: false, title: 'Radio Group', value: 'radio-group' },
+    { disabled: false, title: 'Select', value: 'select' },
+    { disabled: false, title: 'Checkbox', value: 'checkbox' },
+    { disabled: false, title: 'Button', value: 'button' },
+    { disabled: true, title: 'Disabled', value: 'disabled' },
+  ]
+  const componentsExampleArray = [
+    { component: <SignIn />, value: 'sign-in' },
+    { component: <RadioGroup options={radioGroupExampleArray} />, value: 'radio-group' },
+    {
+      component: (
+        <Select
+          ariaLabel={'Select-box'}
+          items={selectExampleArray}
+          placeholder={'Select-box'}
+          title={'Select-box-title'}
+        />
+      ),
+      value: 'select',
+    },
+    {
+      component: (
+        <>
+          <Checkbox
+            id={'c1'}
+            label={'Check-box with label'}
+            name={'c1'}
+            onCheckedChange={e => {
+              console.log(e)
+            }}
+          />
+          <Checkbox disabled id={'c2'} label={'Check-box with label disabled'} name={'c2'} />
+        </>
+      ),
+      value: 'checkbox',
+    },
+    {
+      component: (
+        <>
+          <hr />
+          <div>
+            <Button fullWidth variant={'primary'}>
+              Button Primary Full Width
+            </Button>
+          </div>
+          <hr />
+          <div>
+            <Button variant={'primary'}>Button Primary</Button>
+
+            <Button variant={'primary'}>
+              <Icon iconId={'logout'} />
+              Button Primary Icon
+            </Button>
+
+            <Button disabled variant={'primary'}>
+              Button Primary Disabled
+            </Button>
+          </div>
+          <hr />
+          <div>
+            <Button variant={'secondary'}>Button Secondary</Button>
+            <Button variant={'secondary'}>
+              <Icon iconId={'logout'} />
+              Button Secondary Icon
+            </Button>
+            <Button disabled variant={'secondary'}>
+              Button Secondary Disabled
+            </Button>
+          </div>
+          <hr />
+          <div>
+            <Button as={'a'} href={'/'} variant={'primary'}>
+              <Icon iconId={'logout'} />
+              Button Primary as Link
+            </Button>
+          </div>
+          <hr />
+        </>
+      ),
+      value: 'button',
+    },
   ]
 
   return (
     <div>
       <div>
-        <Tabs
-          defaultValue={'checkbox'}
-          tabs={tabsExampleArray}
-          tabsTitle={'Example title for Tabs'}
-        >
-          <TabContent value={'sign-in'}>
-            <Card>
-              <SignIn />
-            </Card>
-          </TabContent>
-          <TabContent value={'radio-group'}>
-            <Card>
-              <RadioGroup options={radioGroupExampleArray} />
-            </Card>
-          </TabContent>
-          <TabContent value={'select'}>
-            <Card>
-              <Select
-                ariaLabel={'Select-box'}
-                items={selectExampleArray}
-                placeholder={'Select-box'}
-                title={'Select-box-title'}
-              />
-            </Card>
-          </TabContent>
-          <TabContent value={'checkbox'}>
-            <Card>
-              <Checkbox
-                id={'c1'}
-                label={'Check-box with label'}
-                name={'c1'}
-                onCheckedChange={e => {
-                  console.log(e)
-                }}
-              />
-              <Checkbox disabled id={'c2'} label={'Check-box with label disabled'} name={'c2'} />
-            </Card>
-          </TabContent>
-          <TabContent value={'button'}>
-            <Card>
-              <hr />
-              <div>
-                <Button fullWidth variant={'primary'}>
-                  Button Primary Full Width
-                </Button>
-              </div>
-              <hr />
-              <div>
-                <Button variant={'primary'}>Button Primary</Button>
-
-                <Button variant={'primary'}>
-                  <Icon iconId={'logout'} />
-                  Button Primary Icon
-                </Button>
-
-                <Button disabled variant={'primary'}>
-                  Button Primary Disabled
-                </Button>
-              </div>
-              <hr />
-              <div>
-                <Button variant={'secondary'}>Button Secondary</Button>
-                <Button variant={'secondary'}>
-                  <Icon iconId={'logout'} />
-                  Button Secondary Icon
-                </Button>
-                <Button disabled variant={'secondary'}>
-                  Button Secondary Disabled
-                </Button>
-              </div>
-              <hr />
-              <div>
-                <Button as={'a'} href={'/'} variant={'primary'}>
-                  <Icon iconId={'logout'} />
-                  Button Primary as Link
-                </Button>
-              </div>
-              <hr />
-            </Card>
-          </TabContent>
+        <Tabs defaultValue={'select'} tabs={tabsExampleArray} tabsTitle={'Example title for Tabs'}>
+          {componentsExampleArray.map(c => {
+            return (
+              <TabContent key={c.value} value={c.value}>
+                <Card>{c.component}</Card>
+              </TabContent>
+            )
+          })}
         </Tabs>
       </div>
       <hr />
