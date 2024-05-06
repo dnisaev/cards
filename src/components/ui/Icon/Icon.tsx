a8e70243fef1dc075d367ed622c0e67a3a974333
dnisaev <dnisaev@yandex.ru>
2024-05-06 14:36:59 +0300

    fix Icon back

--- src/components/ui/Icon/Icon.tsx
+++ src/components/ui/Icon/Icon.tsx
@@ -11,9 +11,9 @@ export const Icon = ({ height, iconId, viewBox, width }: IconProps) => {
   return (
     <svg
       fill={'none'}
-      height={height || '16'}
-      viewBox={viewBox || '0 0 16 16'}
-      width={width || '16'}
+      height={height || '24'}
+      viewBox={viewBox || '0 0 24 24'}
+      width={width || '24'}
       xmlns={'http://www.w3.org/2000/svg'}
     >
       <use xlinkHref={`${iconsSprite}#${iconId}`} />
